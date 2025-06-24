1ab303885314e52cf135e8c6121f61b237d203c3
pratham-107 <spratham845@gmail.com>
2025-06-24 14:36:48 +0530

    Switched from MySQL to SQLite for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,8 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+});
 
 module.exports = sequelize;
